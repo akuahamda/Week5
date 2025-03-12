a32f4bf8ac714f75ee3dd10cc4932b970a29ea70
akuahamda <hamdalamohammed80@gmail.com>
2025-03-11 17:11:47 -0700

    week 5 assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+//while loop//
+let count = 20;
+while(count > 5){
+    console.log(count);
+    count --
+}
+
+let count2 = 2;
+while(count2 < 30){
+    console.log(count2)
+    count2 ++
+}
+
+let month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+let count3 = 0;
+while(count3 < month.length ){
+    console.log(month[count3]);
+    count3 ++
+}
+
+let name = ["Faisal","Hamda","Faisal","Hamda","Faisal","Hamda","Faisal","Hamda","Faisal","Hamda"];
+let nameCount = 0;
+while(nameCount < name.length){
+    console.log(name[nameCount]);
+    nameCount ++
+}
+
+console.log("While Loop makes you execute a block of code repeatedly as long as the condition is true");
