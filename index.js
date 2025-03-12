3c64aaf78781ebbaafe871c7d0315af738eae54f
akuahamda <hamdalamohammed80@gmail.com>
2025-03-12 16:29:59 -0700

    continue my assigment

--- index.js
+++ index.js
@@ -26,3 +26,31 @@ while(nameCount < name.length){
 }
 
 console.log("While Loop makes you execute a block of code repeatedly as long as the condition is true");
+
+//For Loop//
+
+for(i = 20; i >= 5; i-- ){
+    console.log(i);
+}
+
+for(i = 2; i <= 30; i++){
+    console.log(i);
+}
+
+let mon = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+for(i = 0; i < mon.length; i++){
+    console.log(mon[i]);
+}
+
+let name2 = ["Faisal","Hamda","Faisal","Hamda","Faisal","Hamda","Faisal","Hamda","Faisal","Hamda"];
+for(i = 0; i < name2.length; i++){
+    console.log(name2[i]);
+}
+
+for(i = 1; i <= 10; i++){
+    console.log(i);
+    if(i == 5){
+        console.log("Welcome to loop");
+
+    }
+}
